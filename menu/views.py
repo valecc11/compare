0403eed80a9144d186fc5483b20db06ce752249e
valentina <vale.castroc@duocuc.cl>
2023-11-14 23:54:46 -0300

    ajssa

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'menu/home.html')
+
+def registrar(request):
+    return render(request, 'menu/registrar.html')
+
+def login(request):
+    return render(request, 'menu/login.html')
+
